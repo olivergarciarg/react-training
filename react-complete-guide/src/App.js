147df51961463760fcfeaaa225d182a41ed7c67b
Oliver Garcia <46539206+olivergarciarg@users.noreply.github.com>
2022-01-07 16:59:40 -0600

    start of section 3 video 40

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from './components/ExpenseItem'
+import Expenses from './components/Expenses'
 
 function App() {
   const expenses = [
@@ -29,7 +29,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/>
+      <Expenses items={expenses}/>
     </div>
   )
 }
