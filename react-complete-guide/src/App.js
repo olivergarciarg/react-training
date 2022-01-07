56b0c6727a4e78bf39698a51ce1dd83ba2ab9248
Oliver Garcia <46539206+olivergarciarg@users.noreply.github.com>
2022-01-07 16:45:47 -0600

    section 3 video 39

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -29,7 +29,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
+      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/>
     </div>
   )
 }
