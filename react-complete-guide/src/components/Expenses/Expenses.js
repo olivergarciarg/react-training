6304d9ec94349c5ae01216d669847362ba7d9131
Oliver Garcia <46539206+olivergarciarg@users.noreply.github.com>
2022-01-19 11:32:31 -0600

    start of section 4 video 67

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -11,11 +11,21 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <div>
       <Card className='expenses'>
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-        <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}/>
+        {filteredExpenses.map((expense) => (
+          <ExpenseItem 
+            key={expense.id}
+            title={expense.title} 
+            amount={expense.amount} 
+            date={expense.date}/>
+        ))}
       </Card>
     </div>
   );
