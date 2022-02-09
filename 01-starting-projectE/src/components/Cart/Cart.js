dc5e65e1b42e10f79b555478944c0b10f5710a3e
Oliver Garcia <46539206+olivergarciarg@users.noreply.github.com>
2022-02-09 15:49:53 -0600

    start of section 11 video 146

--- 01-starting-projectE/src/components/Cart/Cart.js
+++ 01-starting-projectE/src/components/Cart/Cart.js
@@ -1,10 +1,14 @@
+import { useContext } from 'react';
+import CartContext from '../../store/cart-context';
 import Modal from '../UI/Modal';
 import classes from './Cart.module.css'
 
 const Cart = props => {
+  const cartCtx = useContext(CartContext);
+
   const cartItems = <ul className={classes['cart-items']}>
     {
-      [{id: 'c1', name: 'Sushi', amount: 2, price: 12.99}].map(item => <li>{item.name}</li>)
+      cartCtx.items.map(item => <li>{item.name}</li>)
     }
   </ul>;
 
