39e35e1a1463cc784af0732698f299cce42a1553
Oliver Garcia <46539206+olivergarciarg@users.noreply.github.com>
2022-02-11 13:09:45 -0600

    start of section 14 video 180

---
+++ 01-starting-projectH/src/App.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+
+import MoviesList from './components/MoviesList';
+import './App.css';
+
+function App() {
+  // const dummyMovies = [
+  //   {
+  //     id: 1,
+  //     title: 'Some Dummy Movie',
+  //     openingText: 'This is the opening text of the movie',
+  //     releaseDate: '2021-05-18',
+  //   },
+  //   {
+  //     id: 2,
+  //     title: 'Some Dummy Movie 2',
+  //     openingText: 'This is the second opening text of the movie',
+  //     releaseDate: '2021-05-19',
+  //   },
+  // ];
+
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  // function fetchMoviesHandler() {
+  //   fetch('https://swapi.dev/api/films').then(response => {
+  //     return response.json();
+  //   }).then(data => {
+  //     const transformedMovies = data.results.map(movie => {
+  //       return {
+  //         id: movie.episode_id,
+  //         title: movie.title,
+  //         openingText: movie.opening_crawl,
+  //         releaseDate: movie.release_date,
+  //       }
+  //     });
+  //     setMovies(transformedMovies);
+  //   });
+  // }
+
+  async function fetchMoviesHandler() {
+    setIsLoading(true);
+    const response = await fetch('https://swapi.dev/api/films')
+    const data = await response.json();
+    const transformedMovies = data.results.map(movie => {
+      return {
+        id: movie.episode_id,
+        title: movie.title,
+        openingText: movie.opening_crawl,
+        releaseDate: movie.release_date,
+      }
+    });
+    setMovies(transformedMovies);
+    setIsLoading(false);
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        { !isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        { !isLoading && movies.length === 0 && <p>Found no movies.</p>}
+        { isLoading && <p>Loading...</p>}
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
