01b65320433303bc22bae0569f00fb405c9cb183
Oliver Garcia <46539206+olivergarciarg@users.noreply.github.com>
2022-02-14 16:48:09 -0600

    start of section 15 video 194

---
+++ 04-onwards-to-a-more-realistic-example/src/hooks/use-http.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+
+const useHttp = (requestConfig, applyData) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  // const [tasks, setTasks] = useState([]);
+
+  const sendRequest = async (taskText) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      // 'https://react-http-6239c-default-rtdb.firebaseio.com/tasks.json'
+      const response = await fetch(requestConfig.url, {
+        method: requestConfig.method || 'GET',
+        headers: requestConfig.headers || {},
+        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null
+      });
+
+      if (!response.ok) {
+        throw new Error('Request failed!');
+      }
+
+      const data = await response.json();
+      applyData(data);
+      
+    } catch (err) {
+      setError(err.message || 'Something went wrong!');
+    }
+    setIsLoading(false);
+  };
+
+  return {
+    isLoading,
+    error,
+    sendRequest,
+  };
+};
+
+export default useHttp;
