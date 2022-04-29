528f3e8ebde9ea4f0be9ae9166e336d0dff8f9e8
Oliver Garcia <46539206+olivergarciarg@users.noreply.github.com>
2022-04-29 16:07:32 -0500

    section 18 video 246

--- 01-starting-projectK/src/components/Counter.js
+++ 01-starting-projectK/src/components/Counter.js
@@ -1,24 +1,25 @@
 // import { Component } from 'react';
 // import { useSelector, useDispatch, connect } from 'react-redux';
 import { useSelector, useDispatch } from 'react-redux';
+import { counterActions } from '../store/index';
 import classes from './Counter.module.css';
 
 const Counter = () => {
   const dispatch = useDispatch();
-  const counter = useSelector(state => state.counter);
-  const showCounter = useSelector(state => state.showCounter);
+  const counter = useSelector(state => state.counter.counter);
+  const showCounter = useSelector(state => state.counter.showCounter);
 
   const incrementHandler = () => {
-    dispatch({ type: 'increment' });
+    dispatch(counterActions.increment());
   };
   const increaseHandler = () => {
-    dispatch({ type: 'increase', amount: 5 });
+    dispatch(counterActions.increase({amount: 5}));
   };
   const decrementHandler = () => {
-    dispatch({ type: 'decrement' });
+    dispatch(counterActions.decrement());
   };
   const toggleCounterHandler = () => {
-    dispatch({ type: 'toggle' });
+    dispatch(counterActions.toggleCounter());
   };
 
   return (
--- 01-starting-projectK/src/store/index.js
+++ 01-starting-projectK/src/store/index.js
@@ -1,38 +1,84 @@
-import { createStore } from 'redux';
+// import { createStore } from 'redux';
+import { createSlice, configureStore } from '@reduxjs/toolkit';
 
-const initialState = {
+const initialCounterState = {
   counter: 0,
   showCounter: true
 };
 
-const counterReducer = (state = initialState, action) => {
-  if (action.type === 'increment') {
-    return {
-      ...state,
-      counter: state.counter + 1
-    }
-  } 
-  if (action.type === 'increase') {
-    return {
-      ...state,
-      counter: state.counter + action.amount
-    }
-  } 
-  if (action.type === 'decrement') {
-    return {
-      ...state,
-      counter: state.counter - 1
-    }
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState: initialCounterState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter += action.payload.amount;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
   }
-  if (action.type === 'toggle') {
-    return {
-      ...state,
-      showCounter: !state.showCounter
-    }
-  }
-  return state;
+});
+
+const initialAuthState = {
+  isAuthenticated: false
 };
 
-const store = createStore(counterReducer);
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  }
+});
+
+// const counterReducer = (state = initialState, action) => {
+//   if (action.type === 'increment') {
+//     return {
+//       ...state,
+//       counter: state.counter + 1
+//     }
+//   } 
+//   if (action.type === 'increase') {
+//     return {
+//       ...state,
+//       counter: state.counter + action.amount
+//     }
+//   } 
+//   if (action.type === 'decrement') {
+//     return {
+//       ...state,
+//       counter: state.counter - 1
+//     }
+//   }
+//   if (action.type === 'toggle') {
+//     return {
+//       ...state,
+//       showCounter: !state.showCounter
+//     }
+//   }
+//   return state;
+// };
+
+// const store = createStore(counterReducer);
+const store = configureStore({
+  reducer: {
+    counter: counterSlice.reducer,
+    auth: authSlice.reducer
+  }
+});
+
+export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 
 export default store;
