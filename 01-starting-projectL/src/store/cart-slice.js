6512da94df78f63d92a92eaabbd8145286189abd
Oliver Garcia <46539206+olivergarciarg@users.noreply.github.com>
2022-05-05 14:48:06 -0500

    section 19 video 253

--- 01-starting-projectL/src/store/cart-slice.js
+++ 01-starting-projectL/src/store/cart-slice.js
@@ -7,12 +7,13 @@ const cartSlice = createSlice({
     totalQuantity: 0,
   },
   reducers: {
-    addItem(state, action) {
+    addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find(item => item.id === newItem.id);
+      state.totalQuantity++;
       if (!existingItem) {
         state.items.push({
-          itemId: newItem.id,
+          id: newItem.id,
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
@@ -23,8 +24,16 @@ const cartSlice = createSlice({
         existingItem.totalPrice += newItem.price;
       }
     },
-    removeItem(state) {
-      state.cartIsVisible = !state.cartIsVisible;
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find(item => item.id === id);
+      state.totalQuantity--;
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter(item => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice -= existingItem.price;
+      }
     }
   }
 });
