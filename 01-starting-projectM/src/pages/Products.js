89804c3598a538997e9bfde3fefad3737b3d6b0b
Oliver Garcia <46539206+olivergarciarg@users.noreply.github.com>
2022-05-10 15:57:05 -0500

    section 20 video 273

---
+++ 01-starting-projectM/src/pages/Products.js
@@ -0,0 +1,12 @@
+const Products = () => {
+  return <section>
+    <h1>The Products page</h1>
+    <ul>
+      <li id='p1'>Book</li>
+      <li id='p2'>Carpet</li>
+      <li id='p3'>Course</li>
+    </ul>
+  </section>;
+};
+
+export default Products;
