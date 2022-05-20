1ee6f18458325ac515009a2cf9a7b88ba3946221
Oliver Garcia <46539206+olivergarciarg@users.noreply.github.com>
2022-05-20 15:48:09 -0500

    section 22 video 310

--- 01-starting-projectP/src/components/Layout/MainNavigation.js
+++ 01-starting-projectP/src/components/Layout/MainNavigation.js
@@ -1,8 +1,18 @@
+import { Fragment, useContext } from 'react';
 import { Link } from 'react-router-dom';
 
+import AuthContext from '../../store/auth-context';
 import classes from './MainNavigation.module.css';
 
 const MainNavigation = () => {
+  const authCtx = useContext(AuthContext);
+
+  const isLoggedIn = authCtx.isLoggedIn;
+
+  const logoutHandler = () => {
+    authCtx.logout();
+  };
+
   return (
     <header className={classes.header}>
       <Link to='/'>
@@ -10,15 +20,35 @@ const MainNavigation = () => {
       </Link>
       <nav>
         <ul>
-          <li>
-            <Link to='/auth'>Login</Link>
-          </li>
-          <li>
-            <Link to='/profile'>Profile</Link>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
+          {
+            isLoggedIn ?
+            <Fragment>
+              <li>
+                <Link to='/profile'>Profile</Link>
+              </li>
+              <li>
+                <button onClick={logoutHandler}>Logout</button>
+              </li>
+            </Fragment>:
+            <li>
+              <Link to='/auth'>Login</Link>
+            </li>
+          }
+          {/* {!isLoggedIn && (
+            <li>
+              <Link to='/auth'>Login</Link>
+            </li>
+          )}
+          {isLoggedIn && (
+            <li>
+              <Link to='/profile'>Profile</Link>
+            </li>
+          )}
+          {isLoggedIn && (
+            <li>
+              <button>Logout</button>
+            </li>
+          )} */}
         </ul>
       </nav>
     </header>
