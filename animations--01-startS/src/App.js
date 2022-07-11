88e4e963bfb14558152c43da44f73f83a157bc77
Oliver Garcia <46539206+olivergarciarg@users.noreply.github.com>
2022-07-11 13:36:06 -0500

    section 24 video 360

--- animations--01-startS/src/App.js
+++ animations--01-startS/src/App.js
@@ -28,7 +28,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        {/* <button
+        <button
           className="Button"
           onClick={() =>
             this.setState((prevState) => ({ showBlock: !prevState.showBlock }))
@@ -36,12 +36,18 @@ class App extends Component {
         >
           Toggle
         </button>
-        <br /> */}
-        {/* <Transition
+        <br />
+        <Transition
           in={this.state.showBlock}
           timeout={1000}
           mountOnEnter
           unmountOnExit
+          onEnter={() => console.log('onEnter')}
+          onEntering={() => console.log('onEntering')}
+          onEntered={() => console.log('onEntered')}
+          onExit={() => console.log('onExit')}
+          onExiting={() => console.log('onExiting')}
+          onExited={() => console.log('onExited')}
         >
           {(state) => (
             <div
@@ -55,7 +61,7 @@ class App extends Component {
               }}
             ></div>
           )}
-        </Transition> */}
+        </Transition>
 
         <Modal closed={this.closeModal} show={this.state.modalIsOpen} />
         {this.state.modalIsOpen && <Backdrop show />}
